996fbad894667a929d8a45522089b2e4655c4700
Joel Reyes <JoellCR07@gmail.com>
2019-10-15 17:54:37 -0600

    Creacion Metodo POST

--- routes/api/fotografias/index.js
+++ routes/api/fotografias/index.js
@@ -12,10 +12,48 @@ router.get('/',function(req, res){
 });
 
 
+//METODO GET
 router.get('/all', function(req,res){
     fotoCollection=fileModel.getFotografias();
     res.json(fotoCollection);
 });
 
+
+//METODO POST
+router.post('/new', function(req,res){
+    fotoCollection=fileModel.getFotografias();
+    var newFotografias=Object.assign(
+        {},
+        req.body,
+        {
+            "title":(req.body.ttle),
+            "url": (req.body.url),
+            "thumbnailUrl": (res.body.thumbnailUrl),
+            "album": (req.body.album)
+        }
+    );
+    var fotoExist=fotoCollection.find(
+        function(o,i){
+            return o.codigo===newFotografias.codigo;
+        }
+    )
+    if(!fotoExist){
+        fotoCollection.push(newFotografias);
+        fileModel.setFotografias(
+            fotoCollection,
+            function(err, savedSuccesfully){
+                if(err){
+                    res.status(400).json({"Error en el aplicativo":"Error"});
+                }else{
+                    res.json(newFotografias);
+                }
+            }
+            );
+
+    }else{
+        res.status(400).json({"Error en el aplicativo":"Error"});
+    }
+
+});
 
 module.exports = router;
